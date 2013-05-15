3e9f5c12c269ff301beb10e35a060a5d589349b5
Arne Bahlo <hallo@arne.me>
2013-05-15 19:09:41 +0200

    Improve comments on exercise 2

--- semester2/practice3/exercise2.c
+++ semester2/practice3/exercise2.c
@@ -1,8 +1,8 @@
-// Lösung zu Termin3
-// Aufgabe 1
-// von:
-// vom: 
-// 
+/**
+ * Provides a signal for the pump
+ * Key 1 stops pumpin
+ * Key 2 starts pumpin
+ */
 
 #include "../h/pmc.h"
 #include "../h/tc.h"
@@ -11,108 +11,114 @@
 
 void taste_irq_handler (void) __attribute__ ((interrupt));
 
-// PUMPIN
-void pumpe_init(void) 
+/**
+  * Initializes pump
+  */
+void pump_init(void)
 {
-  StructPIO* piobaseA  = PIOA_BASE;	// Basisadresse PIO A
-  piobaseA->PIO_OER  = (1<<PIOTIOA3) ;	//enable individiual i/0 signals
-  piobaseA->PIO_CODR = (1<<PIOTIOA3) ;	//pin is used as output, high or low level 
+  StructPIO* piobaseA  = PIOA_BASE;	    // Base address PIOA (for pump pins)
+  piobaseA->PIO_OER  = (1<<PIOTIOA3);	  // Output enable
+  piobaseA->PIO_CODR = (1<<PIOTIOA3);	  // Clear
 }
 
-// PUMPEN
-void pumpe_on(void) 
+/**
+  * Start pump
+  */
+void pump_start(void)
 {
-  StructPIO* piobaseA  = PIOA_BASE;	// Basisadresse PIO A
-  piobaseA->PIO_PDR  = (1<<PIOTIOA3) ;
+  StructPIO* piobaseA  = PIOA_BASE;	    // Base address PIOA (for pump pins)
+  piobaseA->PIO_PDR  = (1<<PIOTIOA3);   // Port disable (switch to pump)
 }
 
-// PUMPDIS
-void pumpe_off(void) 
+/**
+  * Stop pump
+  */
+void pump_stop(void)
 {
-  StructPIO* piobaseA  = PIOA_BASE;	// Basisadresse PIO A
-  piobaseA->PIO_CODR = (1<<PIOTIOA3) ;	//pin is used as output, high or low level 
-  piobaseA->PIO_PER  = (1<<PIOTIOA3) ;
+  StructPIO* piobaseA  = PIOA_BASE;	    // Base address PIOA (for pump pins)
+  piobaseA->PIO_CODR = (1<<PIOTIOA3);	  // Clear (set to low) to prevent permanent high
+  piobaseA->PIO_PER  = (1<<PIOTIOA3);   // Port enable (switch to PIOB)
 }
 
-// Interruptserviceroutine für die Tasten SW1 und SW2
+/**
+ * Interrupt
+ */
 void taste_irq_handler (void)
 {
-  StructPIO* piobaseB   = PIOB_BASE;	// Basisadresse PIO B
-  StructPIO* piobaseA  = PIOA_BASE;	// Basisadresse PIO A
-  StructAIC* aicbase  = AIC_BASE;	//__
-	
-  // KEYS
-  piobaseB->PIO_PER = KEY1 | KEY2 | KEY3; // Port enable
-  piobaseB->PIO_IER = KEY1 | KEY2 | KEY3; // Input enable
-      
+  StructPIO* piobaseB   = PIOB_BASE;	  // Base address PIOB (for controls)
+  StructPIO* piobaseA  = PIOA_BASE;	    // Base address PIOA (for pump pins)
+  StructAIC* aicbase  = AIC_BASE;	      // Base address AIC (for interrupts)
+
+  piobaseB->PIO_PER = KEY1 | KEY2 | KEY3; // Port enable Keys
+  piobaseB->PIO_IER = KEY1 | KEY2 | KEY3; // Input enable keys
+
   if( !( piobaseB->PIO_PDSR & KEY1 ) ) {
-      // Key 1 gedrÃ¼ckt, Pumpe aus
-      pumpe_off();
+      // Key 1 was pressed, stop pump
+      pump_stop();
   } else if( !( piobaseB->PIO_PDSR & KEY2 ) ) {
-      // Key 2 gedrÃ¼ckt, PUMPE an
-      pumpe_on();
+      // Key 2 was pressed, start pump again
+      pump_start();
   }
 
-   aicbase->AIC_EOICR = piobaseB->PIO_ISR;	//end of interrupt command register(handles priorities), updates value of interrupt status register
+   aicbase->AIC_EOICR = piobaseB->PIO_ISR;	// End of interrupt command register (handles priorities), updates value of interrupt status register
 }
 
-// Timer3 initialisieren
-void Timer3_init( void )
+/**
+ * Initialize timer
+ */
+void timer3_init( void )
 {
-  StructTC* timerbase3  = TCB3_BASE;	// Basisadressse TC Block 1
-  StructPIO* piobaseA   = PIOA_BASE;	// Basisadresse PIO A
+  StructTC* timerbase3  = TCB3_BASE;	// Base address TC Block 1
+  StructPIO* piobaseA   = PIOA_BASE;	// Base address PIOA (for pump pins)
 
-  timerbase3->TC_CCR = TC_CLKDIS;	// Disable timer, Channel Control Register 
- 
+  timerbase3->TC_CCR = TC_CLKDIS;	// Disable timer, Channel Control Register
+
   // Initialize the mode of the timer 3
   timerbase3->TC_CMR =
     TC_ACPC_CLEAR_OUTPUT  |    //ACPC    : Register C clear TIOA
     TC_ACPA_SET_OUTPUT    |    //ACPA    : Register A set TIOA
     TC_WAVE               |    //WAVE    : Waveform mode
     TC_CPCTRG             |    //CPCTRG  : Register C compare trigger enable
-    TC_CLKS_MCK8;           //TCCLKS  : MCKI / 1024
+    TC_CLKS_MCK8;              //TCCLKS  : MCKI / 1024
 
   // Initialize the counter:
   timerbase3->TC_RA = 31250;	// cmp register a
   timerbase3->TC_RC = 62500;	// cmp register c
-  
-  //TC_CCR: Software trigger, resets counter and starts counter clock
 
   // Start the timer :
-  timerbase3->TC_CCR = TC_CLKEN ;	// Enable timer again  
-  timerbase3->TC_CCR = TC_SWTRG ;	//set software trigger
- 
-  pumpe_init();
-  pumpe_on();
+  timerbase3->TC_CCR = TC_CLKEN;	// Enable timer again
+  timerbase3->TC_CCR = TC_SWTRG;	// Set software trigger
+
+  pump_init();
+  pump_start();
 }
 
 int main(void)
 {
 
-	StructPMC* pmcbase	= PMC_BASE;	// Basisadresse des PMC
-	StructPIO* piobaseA   	= PIOA_BASE;	// Basisadresse PIO A
-	StructPIO* piobaseB   	= PIOB_BASE;	// Basisadresse PIO B
-	StructAIC* aicbase 	= AIC_BASE; 	// Basisadresse AIC
+	StructPMC* pmcbase	= PMC_BASE;  // Base address PMC
+	StructPIO* piobaseA = PIOA_BASE; // Base address PIOA (for pump pins)
+	StructPIO* piobaseB = PIOB_BASE; // Base address PIO B
+	StructAIC* aicbase  = AIC_BASE;  // Base address AIC
 
-	pmcbase->PMC_PCER	= 0x6200;	// Peripheral Clocks einschalten für PIOB, PIOA, Timer3
-	  
-        // Interrupt initialisieren
+	pmcbase->PMC_PCER	= 0x6200;	// Turn peripheral clocks on for PIOB, PIOA, Timer3
+
+  // Initialize interrupt
 	aicbase->AIC_IDCR = 0x4000;
 	aicbase->AIC_ICCR = 0x4000;
 	aicbase->AIC_SMR[14] = AIC_PRIOR;
 	aicbase->AIC_SVR[14] = taste_irq_handler;
 	aicbase->AIC_IECR = 0x4000;
-	
 	piobaseB->PIO_IER = ALL_KEYS;
 
-	// Timer initialisieren
-	Timer3_init();
-	
-	// Programm Beendung verhindern
+	// Initialize timer
+	timer3_init();
+
+	// Prevent exit
 	while(1)
 	{
-			
+
 	}
-	
+
 	return 0;
 }
